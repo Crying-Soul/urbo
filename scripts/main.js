17b155f24030769545d696e9c7de75407999bd88
egor <grebnevegor2000@mail.ru>
2022-06-06 21:30:41 +0300

    pre fix mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
 "use strict";
 $(document).ready((function() {
+    window.mobileAndTabletCheck = function() {
+        let check = false;
+        (function(a) {
+            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+        })(navigator.userAgent || navigator.vendor || window.opera);
+        return check;
+    };
     let speed = 0,
-        direction = { x: 1, y: 1 };
+        direction = {
+            x: 1,
+            y: 1
+        };
     let bufferX = 0,
         bufferY = 0;
     const copy_model_projects = document.querySelector('#projects .group');
@@ -10,6 +20,28 @@ $(document).ready((function() {
         []
     ];
     let intervalID = null;
+    let counter = 0;
+    let skip = true;
+    const keys = {
+        37: 1,
+        38: 1,
+        39: 1,
+        40: 1
+    };
+    const hrefs_list = [];
+    const mobileScrollBuffer = [];
+    let mobileCounter = 0;
+    let isMobile = window.mobileAndTabletCheck();
+
+    const scroll_counter = 4;
+    const mobile_scroll_counter = 6;
+
+    let href = !localStorage.getItem('href') ? 'home' : localStorage.getItem('href')
+
+    let grid = false;
+
+
+
 
 
     $('.flex-projects').on("mousemove", (e) => {
@@ -101,8 +133,10 @@ $(document).ready((function() {
                                 x: row_buffer[index].x + w,
                                 y: row_buffer[index].y,
                                 original: false
-                            }, );
-                            setTimeout(() => { new_block.style.display = 'flex' }, 1000);
+                            });
+                            setTimeout(() => {
+                                new_block.style.display = 'flex'
+                            }, 1000);
                             parent_el.appendChild(new_block);
 
 
@@ -125,7 +159,9 @@ $(document).ready((function() {
                                 }
 
                             )
-                            setTimeout(() => { new_block.style.display = 'flex' }, 1000);
+                            setTimeout(() => {
+                                new_block.style.display = 'flex'
+                            }, 1000);
                             parent_el.appendChild(new_block);
                         }
                         /**
@@ -147,7 +183,9 @@ $(document).ready((function() {
                                 y: row_buffer[index].y - h,
                                 original: false
                             }]);
-                            setTimeout(() => { new_block.style.display = 'flex' }, 1000);
+                            setTimeout(() => {
+                                new_block.style.display = 'flex'
+                            }, 1000);
                             parent_el.appendChild(new_block);
 
                         }
@@ -173,7 +211,9 @@ $(document).ready((function() {
                                 y: row_buffer[index].y + h,
                                 original: false
                             }]);
-                            setTimeout(() => { new_block.style.display = 'flex' }, 1000);
+                            setTimeout(() => {
+                                new_block.style.display = 'flex'
+                            }, 1000);
                             parent_el.appendChild(new_block);
                         }
                         row_buffer[index].obj.style.transform = `translate(${row_buffer[index].x}px, ${row_buffer[index].y}px)`
@@ -192,17 +232,11 @@ $(document).ready((function() {
         return true;
     }
 
-    const scroll_counter = 4;
-    const mobile_scroll_counter = 6;
 
     /**
      * alternative menu activation
      */
 
-
-    let href = !localStorage.getItem('href') ? 'home' : localStorage.getItem('href')
-
-    let grid = false;
 
     if (href !== 'home') {
         if (href === "about") {
@@ -311,15 +345,18 @@ $(document).ready((function() {
             $('.content').removeClass(`scroll-animation-out-${href} active-content`)
             href = $(this).children().attr('data-href');
             localStorage.setItem('href', href !== "about" ? href : "home")
-            if (href === "about") {
+            if (href === "about" && !isMobile) {
                 location.href = "/pages/about.html";
+
+            } else if (href == "about" && isMobile) {
+
+                $(".mobile-button-view-wrap p").html('close').css("font-size", "20px")
+
             } else if (href === "home") {
                 $(".view-wrapper").addClass("d-n");
             } else {
                 $(".view-wrapper").removeClass("d-n");
             }
-
-
 
             $(this).addClass("active");
             $(`.content#${href}`).addClass(`active-content scroll-animation-in-${href}`);
@@ -361,13 +398,7 @@ $(document).ready((function() {
         }
     }));
 
-    let counter = 0;
-    let skip = true;
-    const keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
-    const hrefs_list = [];
-    const mobileScrollBuffer = [];
-    let mobileCounter = 0;
-    let isMobile = false;
+
 
 
 
@@ -379,8 +410,8 @@ $(document).ready((function() {
 
 
     function preventDefault(e) {
-        if ('changedTouches' in e) {
-            isMobile = true;
+
+        if (isMobile) {
             mobileScrollBuffer.push(e.changedTouches[0].screenY);
 
             if (mobileScrollBuffer.length >= 2 && skip) {
@@ -389,14 +420,14 @@ $(document).ready((function() {
                 mobileScrollBuffer.length = 0;
             }
         } else {
-            isMobile = false;
+
             counter = e.deltaY > 0 ? counter + 1 : counter - 1;
         }
         if ((counter >= scroll_counter || mobileCounter >= mobile_scroll_counter) && hrefs_list[hrefs_list.indexOf(href) + 1] && skip) {
             skip = false;
-            counter, mobileCounter, mobileScrollBuffer.length = 0;
+            mobileScrollBuffer.length = 0;
             $('.content').removeClass(`scroll-animation-out-${href} scroll-animation-in-${href}`)
-            $(`.content#${href}`).addClass(`scroll-animation-out-${href}`)
+            $(`.content#${href}`).addClass(`scroll-animation-out-${href}`);
             setTimeout(() => {
 
                 if ((href === "about" || href === "team") && !isMobile) {
@@ -404,7 +435,7 @@ $(document).ready((function() {
                     location.href = "/pages/about.html";
                 } else if ((href === "about" || href === "team") && isMobile) {
 
-                    $("button.scroll-down p").text('close').css("font-size", "14px")
+                    $(".mobile-button-view-wrap .scroll-down p").text('close').css("font-size", "14px")
 
                 }
                 $(".menu-el").removeClass("active");
@@ -430,8 +461,7 @@ $(document).ready((function() {
             }, 1500);
         }
         if ((counter <= -scroll_counter || mobileCounter <= -mobile_scroll_counter) && hrefs_list[hrefs_list.indexOf(href) - 1] && skip) {
-            counter = 0;
-            mobileCounter = 0;
+
             skip = false;
             $('.content').removeClass(`scroll-animation-out-${href} scroll-animation-in-${href}`)
             $(`.content#${href}`).addClass(`scroll-animation-out-${href}`)
@@ -459,7 +489,6 @@ $(document).ready((function() {
             }, 1500);
 
         }
-
         e.preventDefault();
     }
 
@@ -470,7 +499,9 @@ $(document).ready((function() {
         }
     }
     const supportsPassive = true;
-    const wheelOpt = supportsPassive ? { passive: false } : false;
+    const wheelOpt = supportsPassive ? {
+        passive: false
+    } : false;
     const wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
 
     function disableScroll() {
@@ -486,7 +517,7 @@ $(document).ready((function() {
      * Mobile scroll
      */
 
-    $("button.scroll-down").on("click", (function() {
+    $(".mobile-button-scroll-down").on("click", (function() {
 
         skip = false;
         counter, mobileCounter, mobileScrollBuffer.length = 0;
@@ -496,10 +527,10 @@ $(document).ready((function() {
         setTimeout(() => {
             if (href === "team") {
 
-                $("button.scroll-down p").html('close').css("font-size", "14px");
+                $(".mobile-button-view-wrap p").html('close').css("font-size", "20px")
 
             } else {
-                $("button.scroll-down p").html('<i class="fa-thin fa-arrow-down"></i>').css("font-size", "34px")
+                $(".mobile-button-view-wrap p").html('<i class="fa-thin fa-arrow-down"></i>').css("font-size", "34px")
             }
             $('.content').removeClass(`scroll-animation-out-${href} active-content`)
 
@@ -540,8 +571,9 @@ $(document).ready((function() {
         (function() {
             skip = false;
             $(".mobile-default-view").addClass('d-n');
-            $(".button-scroll-down").addClass('d-n')
-            $(".view-wrapper").addClass('mobile-view-wrapper')
+            $(".mobile-button-scroll-down").addClass('d-n')
+            $(".view-wrapper").removeClass("d-n")
+            $(".view-wrapper").css("display", "block")
             $(".mobile-back-arrow").removeClass('d-n')
 
         })
@@ -549,14 +581,21 @@ $(document).ready((function() {
     $(".mobile-back-arrow").on("click", (function() {
         skip = true
         $(".mobile-default-view").removeClass('d-n');
-        $(".button-scroll-down").removeClass('d-n')
-        $(".view-wrapper").removeClass('mobile-view-wrapper')
+        $(".mobile-button-scroll-down").removeClass('d-n')
+        $(".view-wrapper").css("display", "none")
+        $(".view-wrapper").addClass("d-n")
         $(".mobile-back-arrow").addClass('d-n')
     }));
+    if (isMobile) {
+        updateViewState();
+        $(".view-wrapper").addClass("d-n")
 
+    }
     $(".scroll-down-project").on("click", (function(e) {
         e.preventDefault();
-        $('html,body').animate({ scrollTop: 900 }, 500);
+        $('html,body').animate({
+            scrollTop: 900
+        }, 500);
 
     }));
 }));
